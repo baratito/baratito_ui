7f9d34cc7c5494f51e95bf54ec5ca3fbfa586ad7
Francisco Pitriqueo <franpitri@gmail.com>
2021-06-04 22:17:19 -0300

    ♻️ refactor: make font attributes private in BaratitoIcons

--- lib/icons/baratito_icons.dart
+++ lib/icons/baratito_icons.dart
@@ -1,102 +1,102 @@
 import 'package:flutter/widgets.dart';
 
 abstract class BaratitoIcons {
-  static const fontFamily = 'BaratitoIcons';
-  static const fontPackage = 'baratito_ui';
+  static const _fontFamily = 'BaratitoIcons';
+  static const _fontPackage = 'baratito_ui';
 
   static const IconData arrowDown = IconData(
     0xe801,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData arrowLeft = IconData(
     0xe802,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData arrowRight = IconData(
     0xe803,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData arrowUp = IconData(
     0xe804,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData bag = IconData(
     0xe805,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData buy = IconData(
     0xe806,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData category = IconData(
     0xe807,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData categoryBulk = IconData(
     0xe808,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData close = IconData(
     0xe809,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData editSquare = IconData(
     0xe80a,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData home = IconData(
     0xe80b,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData location = IconData(
     0xe80c,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData notification = IconData(
     0xe80d,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData plus = IconData(
     0xe80e,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData plusSquare = IconData(
     0xe80f,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 
   static const IconData search = IconData(
     0xe810,
-    fontFamily: fontFamily,
-    fontPackage: fontPackage,
+    fontFamily: _fontFamily,
+    fontPackage: _fontPackage,
   );
 }
