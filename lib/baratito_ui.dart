cfe0e33fd36bcc95431085ecd31f7ce2ab3a129f
Francisco Pitriqueo <franpitri@gmail.com>
2021-10-09 20:34:03 -0300

    ♻️ refactor: export some widget packages

--- lib/baratito_ui.dart
+++ lib/baratito_ui.dart
@@ -3,3 +3,6 @@ library baratito_ui;
 export 'icons/baratito_icons.dart';
 export 'src/themes/themes.dart';
 export 'src/ui/ui.dart';
+
+export 'package:auto_size_text/auto_size_text.dart';
+export 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
